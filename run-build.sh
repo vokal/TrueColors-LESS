74a659f572b51fa220b46f17ea0560bfe31d76dc
Jarrett Widman <jarrett@24m2.com>
2016-03-04 14:43:04 -0600

    missed the indent

--- run-build.sh
+++ run-build.sh
@@ -8,7 +8,7 @@ if [ $RESULT == 0 ]; then
   echo "publish coverage for $TRAVIS_COMMIT"
   curl -F coverage=@coverage/lcov.info "https://cvr.vokal.io/coverage?owner=$REPO_OWNER&repo=$REPO_NAME&commit=$TRAVIS_COMMIT&coveragetype=lcov"
 else
-  curl -X POST "https://cvr.vokal.io/coverage/abort?owner=$REPO_OWNER&repo=$REPO_NAME&commit=$GITHASH"
+  curl -X POST "https://cvr.vokal.io/coverage/abort?owner=$REPO_OWNER&repo=$REPO_NAME&commit=$TRAVIS_COMMIT"
 fi
 
 exit $RESULT
